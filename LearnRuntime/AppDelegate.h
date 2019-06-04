cffbc773f59e382b5fa7ab7fa3ce0924bbb1073d
Oliver_Mao <maochengfang@126.com>
2019-06-04 15:44:50 +0800

    Initial Commit

---
+++ LearnRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnRuntime
+//
+//  Created by Mac on 19/6/4.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnRuntime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnRuntime
+//
+//  Created by Mac on 19/6/4.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
